d51da35a4e27faa9d90eb7c09392c8ba8f802296
Imran Abdul <Hitimran@gmail.com>
2017-11-18 22:47:33 +0100

    Update TestInitialAPI.java

--- TestApi/src/main/java/TestInitialAPI.java
+++ TestApi/src/main/java/TestInitialAPI.java
@@ -43,6 +43,7 @@ public class TestInitialAPI {
             String port = System.getProperty("server.port");
             if (port == null) {
                 RestAssured.port = 8080;
+                RestAssured.port = 8080;
             }
             else{
                 RestAssured.port = Integer.valueOf(port);
